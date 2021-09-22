24a5b4e6a7c5ecfeb5c924343eff7f4e1090e7e9
RevaARizky <revaathallah86@gmail.com>
2021-09-22 15:06:13 +0800

    try-to-fix-bug

--- src/Pokemon/PokemonCard.js
+++ src/Pokemon/PokemonCard.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import React, { useState } from 'react'
-import { useEffect } from 'react/cjs/react.development'
+import { useEffect } from 'react'
 import './Pokemon.css'
 
 const PokemonCard = (props) => {
